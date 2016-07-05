2b61208ae2b687b1b4a703ed5af647d0e4fb2870
sunke <sunkejl@live.com>
2016-07-05 16:11:49 +0800

    refs templateMethod

--- Adapters/index.php
+++ Adapters/index.php
@@ -23,3 +23,5 @@
     (new Person())->read(new \Acme\KindleAdapter(new \Acme\Kindle()));
     //适配所有电子书
     (new Person())->read(new \Acme\eReaderAdapter(new \Acme\Note()));
+
+    //An adapter is one of the easier design patterns to learn. The reason why is because you use them in the real world all the time!
