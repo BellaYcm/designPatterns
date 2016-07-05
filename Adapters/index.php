33348e120e8d831d6ce4eba9ebee914355cc5255
sunke <sunkejl@live.com>
2016-07-05 14:21:23 +0800

    refs adapter

---
+++ Adapters/index.php
@@ -0,0 +1,25 @@
+<?php
+
+    /**
+     * Created by PhpStorm.
+     * User: sunke
+     * Date: 2016/7/5
+     * Time: 13:49
+     */
+    require "vendor/autoload.php";
+    use Acme\Book;
+    use Acme\BookInterface;
+
+    class Person
+    {
+        //适配器模式 避免修改这部分代码
+        public function read(BookInterface $book)
+        {
+            $book->open();
+            $book->turnPage();
+        }
+    }
+    //kindle只适配kindle
+    (new Person())->read(new \Acme\KindleAdapter(new \Acme\Kindle()));
+    //适配所有电子书
+    (new Person())->read(new \Acme\eReaderAdapter(new \Acme\Note()));
